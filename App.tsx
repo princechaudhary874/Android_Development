97061998502c0c2c7ae89a6240bd1775779c8446
unknown <princechaudhary0001001@gmail.com>
2023-08-30 22:21:05 +0545

    buttonand onPress event

--- App.tsx
+++ App.tsx
@@ -1,28 +1,37 @@
-import React from "react";
-import { View,Text } from "react-native";
-import CompanyData from "./components/CompanyData";
-const App=()=>{
-  return(
-    <View>
-      <Text style={{fontSize:40,color:'green'}}>Learning the use of components</Text>
-      <Text style={{fontSize:25}}>It is independent and reusable code such as function.some use are given below </Text>
-      <UserData/>
-      <CompanyData/>
-    </View>
-  );
-}
-const UserData=()=>{
-  let name='Prince Chaudhary';
-  const age=20;
-  const aim="Entrepreneur";
-  return(
-    <View>
-      <Text style={{fontSize:20}}>Name: {name}</Text>
-      <Text style={{fontSize:20}}>Age: {age} </Text>
-      <Text style={{fontSize:20}}>Aim: {aim}</Text>
-    </View>
-  );
-}
+import React from 'react';
+import {Button, View, Text} from 'react-native';
 
+
+const App = () => {
+  let data=40;
+  const fruits =(val) => {
+    data=50
+    // console.warn(val);
+    console.warn(data);
+
+   }
+  return (
+    <View>
+      <Text style={{fontSize: 30, color: 'green'}}>
+        Button and onPress Event
+      </Text>
+      <Text style={{fontSize: 50, color: 'black'}}>
+        {data}
+      </Text>
+      {/* trick to call a function using onPress */}
+       {/* this function is mainly used to pass parameter */}
+       <Button
+        title="On Press using arrow function"
+        onPress={() => fruits('kem xo motabhai')}
+        color={'red'}
+      />
+       {/* Alternative tricks */}
+      {/* this method is used when we don't have to pass a parameter */}
+      <Button title="On Press" onPress={fruits} color={'green'} />
+     
+     
+    </View>
+  );
+};
 
 export default App;
