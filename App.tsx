9df0e76fb6f7ecc0af87a97803c718568145787e
unknown <princechaudhary0001001@gmail.com>
2023-08-30 09:41:05 +0545

    text,view and button core components of reat-native

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
-import { Text,View } from "react-native";
+import React from "react";
+import{
+  View,
+  Text,
+  Button
+}from 'react-native';
 
 const App=()=>{
   return(
     <View>
-      <Text style={{fontSize:29}}>Hello in the world of hacking!</Text>
-    </View>
-  )
+    <Text style={{fontSize:40}}>Hello React Native</Text>
+    <Text style={{fontSize:30}}>Hello Prince Chaudhary</Text>
+    {/* button can also be created within self closing tag  but title must be included*/}
+    <Button title='Press Me' color={'purple'}/>
+  </View>
+  );
+ 
 }
-
 export default App;
