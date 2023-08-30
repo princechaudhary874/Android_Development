17cf4253f897d431192afa804f19cbd121f2e2e3
unknown <princechaudhary0001001@gmail.com>
2023-08-30 09:08:19 +0545

    Android development first day

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { Text,View } from "react-native";
+
+const App=()=>{
+  return(
+    <View>
+      <Text style={{fontSize:29}}>Hello in the world of hacking!</Text>
+    </View>
+  )
+}
+
+export default App;
