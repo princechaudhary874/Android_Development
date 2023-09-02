7981c0548fb4a37d76e9f41ba9b4274c1aa6fd22
unknown <princechaudhary0001001@gmail.com>
2023-09-02 17:14:23 +0545

    Style of react-native

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-
+import { StyleSheet, Text, View } from 'react-native'
+// importing external Style
+import ExStyles from './components/style'
 const App = () => {
   return (
     <View>
-      <Text>Style: used to design</Text>
+      {/* inline style */}
+      <Text style={{fontSize:40, backgroundColor:'blue'}} >Style: used to design</Text>
+      <Text  style={styles.textBox}>Internal Style</Text>
+      <Text  style={ExStyles.exTextBox}>External Style</Text>
+      {/* using  inline,internal,external style on a single text
+      */}
+      <Text  style={[ExStyles.exTextBox,styles.textBox,{fontWeight:'300'}]}>All in one.</Text>
     </View>
   )
 }
-
+// internal css
+const styles=StyleSheet.create(
+  {
+    textBox:{
+      fontSize:30,
+      backgroundColor:'green',
+      marginTop:10,
+      textAlign:'center',
+      textAlignVertical:'center',
+      borderRadius:20,
+      height:50,
+      color:'white'
+    }
+  }
+)
 export default App
---
+++ components/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+//External Style
+const ExStyles=StyleSheet.create(
+    {
+      exTextBox:{
+        fontSize:30,
+        backgroundColor:'purple',
+        marginTop:10,
+        textAlign:'center',
+        textAlignVertical:'center',
+        borderRadius:20,
+        height:50,
+        color:'white'
+      }
+    }
+  )
+
+  export default ExStyles;
