ed1b31d095a218ae7c3eec3bfaec03ae9346e58d
unknown <princechaudhary0001001@gmail.com>
2023-09-02 16:07:56 +0545

    Learning style

--- App.tsx
+++ App.tsx
@@ -1,25 +1,12 @@
-import React, {useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
 
 const App = () => {
-  const [name, setName] = useState('I am the child components');
   return (
     <View>
-      <Text style={{fontSize: 40}}>Props</Text>
-      <Button
-        title="Update Props"
-        onPress={() => setName('Props is updated successfull!')}
-      />
-      <User name={name} />
+      <Text>Style: used to design</Text>
     </View>
-  );
-};
-//creating child components
-const User = (props) => {
-  return (
-    <View style={{backgroundColor: 'black', paddingLeft: 25}}>
-      <Text style={{fontSize: 30, color: 'aqua'}}>{props.name}</Text>
-    </View>
-  );
-};
-export default App;
+  )
+}
+
+export default App
