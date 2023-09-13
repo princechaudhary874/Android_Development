a1042b66600f63b59b85ccb79c0bbff9afa1a04b
princechaudhary874 <princechaudhary0001001@gmail.com>
2023-09-14 00:11:44 +0545

    List with map Function

--- App.jsx
+++ App.jsx
@@ -1,45 +1,54 @@
 import React from 'react';
-import {FlatList, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
   const users = [
-    {id: 1, name: 'Sagar'},
-    {
-      id: 2,
-      name: 'Raj',
-    },
-    {
-      id: 3,
-      name: 'Prince',
-    },
-    {
-      id: 4,
-      name: 'goal',
-    },
+    {id: 1, name: 'Ranjit'},
+    {id: 4, name: 'Sagar'},
+    {id: 6, name: 'Sneha'},
+    {id: 3, name: 'Prince'},
+    {id: 10, name: 'Rajesh'},
+    {id: 14, name: 'Sangam'},
+    {id: 16, name: 'Neha'},
+    {id: 13, name: 'Princess'},
+    {id: 12, name: 'Raju'},
+    {id: 24, name: 'Sanjay'},
+    {id: 26, name: 'Sontosh'},
+    {id: 23, name: 'Priya'},
+    {id: 210, name: 'Raja'},
+    {id: 214, name: 'Satish'},
+    {id: 216, name: 'Nitish'},
+    {id: 213, name: 'Pankaj'},
   ];
   return (
-    <View>
-      <Text style={{fontSize: 32}}>List with Flat List component</Text>
-      <FlatList
-        data={users}
-        renderItem={({item}) => (
-          <Text
-            style={{
-              fontSize: 20,
-              backgroundColor: 'black',
-              color: 'aqua',
-              marginBottom: 10,
-              textAlign: 'center',
-              textAlignVertical:'center',
-              borderRadius:20
-            }}>
-            {item.name}
-          </Text>
-        )}
-        keyExtractor={item => item.id}
-      />
+    <View >
+      <Text style={styles.heading}>List with Map Function</Text>
+       <ScrollView>
+       {users.map(item => (
+          <Text style={styles.mapComp}>{item.name}</Text>
+        ))}
+       </ScrollView>
+      
     </View>
   );
 };
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 32,
+    backgroundColor: 'black',
+    color: 'white',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
 
+  mapComp: {
+    fontSize: 32,
+    backgroundColor: 'black',
+    color: 'aqua',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    borderRadius: 20,
+    marginBottom: 10,
+  },
+});
 export default App;
