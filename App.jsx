7efa78d0dbd8a287e70f47262369751d4eb2b2c6
princechaudhary874 <princechaudhary0001001@gmail.com>
2023-09-14 01:03:23 +0545

    Dynamic Grid

--- App.jsx
+++ App.jsx
@@ -7,28 +7,31 @@ const App = () => {
     {id: 4, name: 'Sagar'},
     {id: 6, name: 'Sneha'},
     {id: 3, name: 'Prince'},
-    {id: 10, name: 'Rajesh'},
-    {id: 14, name: 'Sangam'},
-    {id: 16, name: 'Neha'},
+    {id: 9, name: 'Rajesh'},
+    {id: 10, name: 'Sangam'},
+    {id: 11, name: 'Neha'},
     {id: 13, name: 'Princess'},
     {id: 12, name: 'Raju'},
-    {id: 24, name: 'Sanjay'},
-    {id: 26, name: 'Sontosh'},
-    {id: 23, name: 'Priya'},
-    {id: 210, name: 'Raja'},
-    {id: 214, name: 'Satish'},
-    {id: 216, name: 'Nitish'},
-    {id: 213, name: 'Pankaj'},
+    {id: 14, name: 'Sanjay'},
+    {id: 16, name: 'Sontosh'},
+    {id: 18, name: 'Priya'},
+    {id: 20, name: 'Raja'},
+    {id: 21, name: 'Satish'},
+    {id: 22, name: 'Nitish'},
+    {id: 23, name: 'Pankaj'},
   ];
   return (
-    <View >
-      <Text style={styles.heading}>List with Map Function</Text>
-       <ScrollView>
-       {users.map(item => (
-          <Text style={styles.mapComp}>{item.name}</Text>
-        ))}
-       </ScrollView>
-      
+    <View style={{flex: 1}}>
+      <Text style={styles.heading}>Dynamic Grid</Text>
+      <ScrollView>
+        <View style={styles.gridContainer}>
+          {users.map(item => (
+            <View key={item.id} style={styles.gridItem}>
+              <Text style={styles.mapComp}>{item.name}</Text>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -40,15 +43,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginBottom: 20,
   },
-
+  gridContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  gridItem: {
+    width: '50%', // You can adjust the width as needed for the desired number of columns
+  },
   mapComp: {
-    fontSize: 32,
+    fontSize: 30,
     backgroundColor: 'black',
     color: 'aqua',
     textAlign: 'center',
     textAlignVertical: 'center',
     borderRadius: 20,
-    marginBottom: 10,
+    margin: 10,
+    width: 150, 
+    height: 150,
   },
 });
 export default App;
