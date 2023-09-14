e75fbadcf59fdac772eca68581f1cda3527220d5
princechaudhary874 <princechaudhary0001001@gmail.com>
2023-09-14 09:24:57 +0545

    Loop with Flat List

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
   const users = [
@@ -21,17 +21,24 @@ const App = () => {
     {id: 23, name: 'Pankaj'},
   ];
   return (
-    <View style={{flex: 1}}>
-      <Text style={styles.heading}>Dynamic Grid</Text>
-      <ScrollView>
-        <View style={styles.gridContainer}>
-          {users.map(item => (
-            <View key={item.id} style={styles.gridItem}>
-              <Text style={styles.mapComp}>{item.name}</Text>
-            </View>
-          ))}
-        </View>
-      </ScrollView>
+    <View>
+      <Text style={styles.heading}>Loop with flat list</Text>
+      {/* FlatList component to render the list of users */}
+      <FlatList
+        data={users}
+        renderItem={({item}) => <UserData items={item} />}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  );
+};
+const UserData = props => {
+  //Access props from the flatlist
+  const item = props.items;
+  return (
+    <View style={styles.box}>
+      <Text style={styles.items}>{item.id}.</Text>
+      <Text style={styles.items}>{item.name}</Text>
     </View>
   );
 };
@@ -43,23 +50,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginBottom: 20,
   },
-  gridContainer: {
+  box: {
     flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  gridItem: {
-    width: '50%', // You can adjust the width as needed for the desired number of columns
-  },
-  mapComp: {
-    fontSize: 30,
     backgroundColor: 'black',
+    marginBottom: 10,
+    borderRadius: 20,
+    borderWidth: 3,
+    borderColor: 'aqua',
+    height: 50,
+    marginLeft: 25,
+    marginRight: 25,
+  },
+  items: {
+    flex: 1,
+    fontSize: 20,
     color: 'aqua',
     textAlign: 'center',
     textAlignVertical: 'center',
-    borderRadius: 20,
-    margin: 10,
-    width: 150, 
-    height: 150,
   },
 });
 export default App;
